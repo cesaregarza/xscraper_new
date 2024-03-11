f8443d738fb746a29f9c575ab805b85093c853fa
Cesar E Garza <cesar@cegarza.com>
2024-03-10 19:24:35 -0500

    Refactor database functions and add type hints

--- src/xscraper/scraper/db.py
+++ src/xscraper/scraper/db.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import os
+from typing import TYPE_CHECKING
 
 import psycopg2
 from psycopg2.extras import execute_values
@@ -7,10 +10,13 @@ from xscraper.scraper.sql import (
     INSERT_PLAYER_QUERY,
     INSERT_SCHEDULE_QUERY,
     SELECT_CURRENT_SCHEDULE_QUERY,
-    SELECT_PREVIOUS_SCHEDULE_QUERY,
     SELECT_MAX_TIMESTAMP_AND_MODE_QUERY,
+    SELECT_PREVIOUS_SCHEDULE_QUERY,
 )
 from xscraper.scraper.types import Player, Schedule
+
+if TYPE_CHECKING:
+    from psycopg2.extensions import connection as Connection
 
 
 def get_db_credentials() -> dict[str, str]:
@@ -23,74 +29,70 @@ def get_db_credentials() -> dict[str, str]:
     }
 
 
-def get_db_connection() -> psycopg2.extensions.connection:
+def get_db_connection() -> Connection:
     return psycopg2.connect(**get_db_credentials())
 
 
-def insert_players(players: list[Player]) -> None:
-    with get_db_connection() as conn:
-        with conn.cursor() as cursor:
-            values = [
-                (
-                    player["id"],
-                    player["name"],
-                    player["name_id"],
-                    player["rank"],
-                    player["x_power"],
-                    player["weapon_id"],
-                    player["nameplate_id"],
-                    player["byname"],
-                    player["text_color"],
-                    player.get("badge_left_id"),
-                    player.get("badge_center_id"),
-                    player.get("badge_right_id"),
-                    player["timestamp"],
-                    player["mode"],
-                    player["region"],
-                    player["rotation_start"],
-                    player["season_number"],
-                )
-                for player in players
-            ]
-            execute_values(cursor, INSERT_PLAYER_QUERY, values)
+def insert_players(conn: Connection, players: list[Player]) -> None:
+    with conn.cursor() as cursor:
+        values = [
+            (
+                player["id"],
+                player["name"],
+                player["name_id"],
+                player["rank"],
+                player["x_power"],
+                player["weapon_id"],
+                player["nameplate_id"],
+                player["byname"],
+                player["text_color"],
+                player.get("badge_left_id"),
+                player.get("badge_center_id"),
+                player.get("badge_right_id"),
+                player["timestamp"],
+                player["mode"],
+                player["region"],
+                player["rotation_start"],
+                player["season_number"],
+            )
+            for player in players
+        ]
+        execute_values(cursor, INSERT_PLAYER_QUERY, values)
 
 
-def insert_schedule(schedules: list[Schedule]) -> None:
-    with get_db_connection() as conn:
-        with conn.cursor() as cursor:
-            values = [
-                (
-                    schedule["start_time"],
-                    schedule["end_time"],
-                    schedule["splatfest"],
-                    schedule.get("mode"),
-                    schedule.get("stage_1_id"),
-                    schedule.get("stage_1_name"),
-                    schedule.get("stage_2_id"),
-                    schedule.get("stage_2_name"),
-                )
-                for schedule in schedules
-            ]
-            execute_values(cursor, INSERT_SCHEDULE_QUERY, values)
+def insert_schedule(conn: Connection, schedules: list[Schedule]) -> None:
+    with conn.cursor() as cursor:
+        values = [
+            (
+                schedule["start_time"],
+                schedule["end_time"],
+                schedule["splatfest"],
+                schedule.get("mode"),
+                schedule.get("stage_1_id"),
+                schedule.get("stage_1_name"),
+                schedule.get("stage_2_id"),
+                schedule.get("stage_2_name"),
+            )
+            for schedule in schedules
+        ]
+        execute_values(cursor, INSERT_SCHEDULE_QUERY, values)
 
 
-def get_schedule(previous: bool = False) -> Schedule:
+def get_schedule(conn: Connection, previous: bool = False) -> Schedule:
     query = (
         SELECT_PREVIOUS_SCHEDULE_QUERY
         if previous
         else SELECT_CURRENT_SCHEDULE_QUERY
     )
-    with get_db_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(query)
-            value = cursor.fetchone()
-            if value is None:
-                return None
-            return Schedule(*value)
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        value = cursor.fetchone()
+        if value is None:
+            return None
+        return Schedule(*value)
 
 
-def get_latest_timestamp() -> str:
-    with get_db_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(SELECT_MAX_TIMESTAMP_AND_MODE_QUERY)
-            return cursor.fetchone()
+def get_latest_timestamp(conn: Connection) -> str:
+    with conn.cursor() as cursor:
+        cursor.execute(SELECT_MAX_TIMESTAMP_AND_MODE_QUERY)
+        return cursor.fetchone()
--- src/xscraper/scraper/main.py
+++ src/xscraper/scraper/main.py
@@ -1,10 +1,15 @@
+from __future__ import annotations
+
 import datetime as dt
 import os
+from typing import TYPE_CHECKING
 
+import pytz
 from dotenv import load_dotenv
 from splatnet3_scraper.query import QueryHandler
 
 from xscraper import constants as xc
+from xscraper.scraper.db import get_db_connection, get_schedule
 from xscraper.scraper.scrape import get_schedule, scrape_all_players_in_mode
 from xscraper.scraper.types import Player, Schedule
 from xscraper.scraper.utils import (
@@ -12,9 +17,24 @@ from xscraper.scraper.utils import (
     pull_previous_schedule,
 )
 
+if TYPE_CHECKING:
+    from psycopg2.extensions import connection as Connection
+
+
+def calculate_modes_to_update(
+    timestamp: dt.datetime, connection: Connection
+) -> list[Schedule]:
+    out = [get_schedule(connection)]
+    if pull_previous_schedule(timestamp):
+        out.append(pull_previous_schedule(timestamp))
+    return out
+
 
 def scrape(scraper: QueryHandler) -> list[Player]:
-    players = []
+    utc_tz = pytz.timezone("UTC")
+    timestamp = utc_tz.localize(dt.datetime.now())
+    players: list[Player] = []
+    conn = get_db_connection()
 
 
 def main() -> None:
