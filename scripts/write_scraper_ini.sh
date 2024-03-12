866c12f28e6c155676ca760374c27a69836de33e
Cesar E Garza <cesar@cegarza.com>
2024-03-12 01:43:36 -0500

    Add setup_dev_db.sh and write_scraper_ini.sh scripts

---
+++ scripts/write_scraper_ini.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IFS=', ' read -ra ADDR <<< "$1"
+
+for i in "${ADDR[@]}"; do
+    filename="SCRAPER_${i}.ini"
+    echo "[tokens]" > $filename
+    echo "session_token = ${ADDR[$i]}" >> $filename
+    echo "gtoken = randomText" >> $filename
+    echo "bullet_token = randomText" >> $filename
+    echo "" >> $filename
+    echo "[data]" >> $filename
+    echo "country = US" >> $filename
+    echo "language = en-US" >> $filename
+    echo "" >> $filename
+    echo "[options]" >> $filename
+    echo -e "f_token_url = https://nxapi-znca-api.fancy.org.uk/api/znca/f,https://api.imink.app/f" >> $filename
+done
