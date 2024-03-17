daa5b4f96b0640988f2b2615821dcbb0e3654fb1
Cesar E Garza <cesar@cegarza.com>
2024-03-16 23:59:33 -0500

    Update scraping cadence and offset

--- src/xscraper/variables.py
+++ src/xscraper/variables.py
@@ -1,8 +1,8 @@
 import datetime as dt
 
 # Defineable constants
-SCRAPE_CADENCE = dt.timedelta(minutes=15)
-SCRAPE_OFFSET_MINUTES = dt.timedelta(minutes=6)
+SCRAPE_CADENCE = dt.timedelta(minutes=10)
+SCRAPE_OFFSET_MINUTES = dt.timedelta(minutes=4)
 LOG_FILE_PATH = "logs/scraping.log"
 LOG_MAX_BYTES = 1024 * 1024  # 1MB
 LOG_BACKUP_COUNT = 5
